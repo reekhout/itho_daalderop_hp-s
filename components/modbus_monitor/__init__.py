3ab30690f3e7377d17d2fafccb16086fa8d9e200
reekhout <r.eekhout@gmail.com>
2025-08-05 14:43:27 +0200

    Update __init__.py

--- components/modbus_monitor/__init__.py
+++ components/modbus_monitor/__init__.py
@@ -1,32 +1,18 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.cpp_helpers import gpio_pin_expression
-from esphome.const import (CONF_ID, CONF_FLOW_CONTROL_PIN)
 from esphome.components import uart
-from esphome import pins
-
-CODEOWNERS = ["@reekhout"]
+from esphome.const import CONF_ID
 
 DEPENDENCIES = ["uart"]
-AUTO_LOAD = ["binary_sensor"]
-MULTI_CONF = True
 
-modbus_monitor_ns = cg.esphome_ns.namespace('modbus_monitor')
-
+modbus_monitor_ns = cg.esphome_ns.namespace("modbus_monitor")
 ModbusMonitor = modbus_monitor_ns.class_('ModbusMonitor', cg.Component, uart.UARTDevice)
-CONF_LOG_NOT_CONFIGURED_DATA = 'log_not_configured_data'
 
 CONFIG_SCHEMA = cv.Schema({
-  cv.GenerateID(): cv.declare_id(ModbusMonitor),
-  cv.Optional(CONF_LOG_NOT_CONFIGURED_DATA, default=False): cv.boolean,
-  cv.Optional(CONF_FLOW_CONTROL_PIN): pins.gpio_output_pin_schema
+  cv.GenerateID(): cv.declare_id(ModbusMonitor)
 }).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
 
 async def to_code(config):
-  rhs = ModbusMonitor.new(config[CONF_LOG_NOT_CONFIGURED_DATA])
-  var = cg.Pvariable(config[CONF_ID], rhs)
+  var = cg.new_Pvariable(config[CONF_ID])
   await cg.register_component(var, config)
   await uart.register_uart_device(var, config)
-  if CONF_FLOW_CONTROL_PIN in config:
-    pin = await gpio_pin_expression(config[CONF_FLOW_CONTROL_PIN])
-    cg.add(var.set_flow_control_pin(pin))
